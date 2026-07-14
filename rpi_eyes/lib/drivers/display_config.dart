c26dbc6707f6fe30b79d05a15115ca23d808d2a7
ilker <ilker@okutman.com>
2026-07-14 11:55:58 +0300

    Auto-deployment commit - 2026-07-14 11:55:58

--- rpi_eyes/lib/drivers/display_config.dart
+++ rpi_eyes/lib/drivers/display_config.dart
@@ -8,9 +8,8 @@ class DisplayConfig {
   static const int bytesPerPixel = 2;
   static const int bufferSize = width * height * bytesPerPixel;
 
-  // 4 MHz balances speed and signal integrity with jumper-wire wiring.
-  // Lower to 2 MHz or 1 MHz if the image becomes garbled.
-  static const int spiSpeedHz = 4000000;
+  // 8 MHz for better frame rate. Lower to 4/2/1 MHz if the image corrupts.
+  static const int spiSpeedHz = 8000000;
   static const int spiBus = 0;
 
   // CS: Pin 24 = CE0, Pin 26 = CE1
